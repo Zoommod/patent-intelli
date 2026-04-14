c422728b8fa47329ea25f84a7095d5a67b42cf02
Zoommod <vitor.sacramento09@gmail.com>
2026-04-14 15:37:19 -0300

    feat: configura interface de administracao dos modelos

--- mineracao/admin.py
+++ mineracao/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Empresa, Patente
 
-# Register your models here.
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ('cnpj', 'razao_social')
+    search_fields = ('cnpj', 'razao_social')
+ 
+@admin.register(Patente)
+class PatenteAdmin(admin.ModelAdmin):
+    list_display = ('numero_registro', 'titulo', 'empresa')
+    search_fields = ('numero_registro', 'titulo', 'empresa__cnpj', 'empresa__razao_social')
+
